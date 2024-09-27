9169f09d6fe1c6038b038738efba5de78a32c84e
inhee <devforplayer@gmail.com>
2024-09-27 15:23:16 +0900

    v0.3 typeof

--- week05/main.go
+++ week05/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"fmt" //#include <stdio.h>
 	"math"
+	"reflect"
 	"strings"
 )
 
 func main() {
+	//명확하게 정의할 때 사용
 	//var i int = 55
-	var f float32 = 1.1
+	//var i int
+	//i = 55
+	//var f float32 = 1.1
 
-	i := 55
+	//i := 55
+	i := "55"
+	f := 1.1 //64로 됨
 
-	fmt.Printf("%d\n", i)
+	fmt.Printf("%s\n", i)
 	fmt.Print("1은 ", i, "\n")
 	fmt.Println("1은", i)
 	fmt.Println("2은", f, math.Ceil(3.49))
 	fmt.Println(strings.Title("new title"))
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 }
