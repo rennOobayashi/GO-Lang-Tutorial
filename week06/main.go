3334fb00bb9c1f34bce7a40589ce01b188ff63ca
inhee <devforplayer@gmail.com>
2024-10-04 15:03:56 +0900

    v0.6 unicode (runes)

--- week06/main.go
+++ week06/main.go
@@ -6,13 +6,16 @@ import (
 )
 
 func main() {
-	//var i int = 13
-	i := 13
-	var f float64 = 12.9
+	i := 13              //var i int = 13
+	var f float64 = 12.9 //f := 12.9
+	c1 := '1'            //49
+	c2 := 'あ'            //12354 unicode.org
 
 	fmt.Printf("value i: %d, value f: %f\n", i, f)
 	//fmt.Printf("%d + %f = %f\n", i, f, i*f)
 	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
 	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
 	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+	fmt.Println(c1, c2)
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
 }
