523a49b705de2d43a2c697795e39074e412a7a18
inhee <devforplayer@gmail.com>
2024-10-11 15:27:40 +0900

    v0.13 Parse

--- week07/main.go
+++ week07/main.go
@@ -5,31 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	fmt.Print("Input ur score: ")
 	in := bufio.NewReader(os.Stdin)
-	fmt.Println("Input ur name: ")
-	name, err := in.ReadString('\n')
+	i, err := in.ReadString('\n')
 
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Println(name)
 	}
-	// fmt.Println(err)
 
-	// var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다."
-	// armyfixed := strings.NewReplacer("$", "국")
-	// fmt.Println(army)
-	// fmt.Println(armyfixed.Replace(army))
+	i = strings.TrimSpace(i)
+	score, _ := strconv.ParseInt(i, 16, 32)
+	fmt.Printf("%d ", score)
 
-	// var now time.Time = time.Now()
-	// year := now.Year()
-	// month := int(now.Month())
-	// days := now.Day()
+	if score >= 60 {
+		fmt.Println("a")
+	} else {
+		fmt.Println("BCDF")
+	}
 
-	// //오늘은 2024년 10월 11일 입니다.
-	// fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", year, month, days)
-	// fmt.Printf("지금은 %d시 %d분 %d초 입니다.\n", now.Hour(), now.Minute(), now.Second())
 }
