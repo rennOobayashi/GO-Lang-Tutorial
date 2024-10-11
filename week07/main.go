61c566298bc52b5752680461c915ed74eedd82c1
inhee <devforplayer@gmail.com>
2024-10-11 14:01:01 +0900

    v0.9 time func2

--- week07/main.go
+++ week07/main.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 	var now time.Time = time.Now()
-	var month int = int(now.Month())
+	year := now.Year()
+	month := int(now.Month())
+	days := now.Day()
 
-	fmt.Println(month)
+	//오늘은 2024년 10월 11일 입니다.
+	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", year, month, days)
+	fmt.Printf("지금은 %d시 %d분 %d초 입니다.\n", now.Hour(), now.Minute(), now.Second())
 }
