aa003902a748927212157e9ccae00e153b413a42
inhee <devforplayer@gmail.com>
2024-10-25 14:29:52 +0900

    v0.17 guess game *for

--- week09/main.go
+++ week09/main.go
@@ -16,23 +16,25 @@ func main() {
 	answer := rand.Intn(6)
 	fmt.Println(answer)
 
-	fmt.Print("숫자입력: ")
-	in := bufio.NewReader(os.Stdin)
-	i, err := in.ReadString('\n')
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Print("숫자입력: ")
+		in := bufio.NewReader(os.Stdin)
+		i, err := in.ReadString('\n')
 
-	if err != nil {
-		log.Fatal(err)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		i = strings.TrimSpace(i)
+		guess, _ := strconv.Atoi(i)
+
+		if answer == guess {
+			fmt.Println("입력하신 수는 정답입니다.")
+		} else if answer > guess {
+			fmt.Println("입력하신 수는 정답보다 작은 수 입니다.")
+		} else {
+			fmt.Println("입력하신 수는 정답보다 큰 수 입니다.")
+		}
 	}
 
-	i = strings.TrimSpace(i)
-	guess, _ := strconv.Atoi(i)
-
-	if answer == guess {
-		fmt.Println("입력하신 수는 정답입니다.")
-	} else if answer > guess {
-		fmt.Println("입력하신 수는 정답보다 작은 수 입니다.")
-	} else {
-		fmt.Println("입력하신 수는 정답보다 큰 수 입니다.")
-	}
-	fmt.Println(guess)
 }
