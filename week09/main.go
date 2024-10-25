6f10ea3a31286f03455e29cad04edaf2e236f4d3
inhee <devforplayer@gmail.com>
2024-10-25 14:25:04 +0900

    v0.16 guess game1

--- week09/main.go
+++ week09/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	target := rand.Intn(6)
-	fmt.Println(target)
+	answer := rand.Intn(6)
+	fmt.Println(answer)
+
+	fmt.Print("숫자입력: ")
+	in := bufio.NewReader(os.Stdin)
+	i, err := in.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)
+	guess, _ := strconv.Atoi(i)
+
+	if answer == guess {
+		fmt.Println("입력하신 수는 정답입니다.")
+	} else if answer > guess {
+		fmt.Println("입력하신 수는 정답보다 작은 수 입니다.")
+	} else {
+		fmt.Println("입력하신 수는 정답보다 큰 수 입니다.")
+	}
+	fmt.Println(guess)
 }
