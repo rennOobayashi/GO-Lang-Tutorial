d05e17405f738bc45cb6289962de5f8ca35a4eee
inhee <devforplayer@gmail.com>
2024-10-25 14:56:48 +0900

    v0.18 guess game complete

--- week09/main.go
+++ week09/main.go
@@ -12,12 +12,14 @@ import (
 )
 
 func main() {
+	var win bool = false
+
 	rand.Seed(time.Now().Unix())
 	answer := rand.Intn(6)
 	fmt.Println(answer)
 
 	for guesses := 0; guesses < 3; guesses++ {
-		fmt.Print("숫자입력: ")
+		fmt.Printf("숫자입력(남은 기회 %d회): ", 3-guesses)
 		in := bufio.NewReader(os.Stdin)
 		i, err := in.ReadString('\n')
 
@@ -30,11 +32,20 @@ func main() {
 
 		if answer == guess {
 			fmt.Println("입력하신 수는 정답입니다.")
+
+			win = true
+			break
 		} else if answer > guess {
 			fmt.Println("입력하신 수는 정답보다 작은 수 입니다.")
 		} else {
 			fmt.Println("입력하신 수는 정답보다 큰 수 입니다.")
 		}
+	}
+
+	if win {
+		fmt.Println("\n********************\n*당신이 이겼습니다.*\n********************")
+	} else {
+		fmt.Println("\n------------------\n|당신이 졌습니다.|\n------------------")
 	}
 
 }
