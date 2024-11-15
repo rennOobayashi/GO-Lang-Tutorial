53278d38cf9bf3a6df218063f4fb604c507da98c
inhee <devforplayer@gmail.com>
2024-11-15 15:16:01 +0900

    v5.2 array input

--- week12/main.go
+++ week12/main.go
@@ -2,35 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+
+	"github.com/headfirstgo/keyboard"
 )
 
 func main() {
-	var scores [3]int
+	var gpa [3]float64
 
-	scores[1] = 90
+	for i := 0; i < len(gpa); i++ {
+		fmt.Printf("input float number: ")
+		s, err := keyboard.GetFloat()
 
-	//for i := 0; i < len(scores); i++ //<-panic: runtime error: index out of range [3] with length 3
-	for i := 0; i < len(scores); i++ { //unsafe
-		fmt.Printf("%d ", scores[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		gpa[i] = s
+
 	}
 
-	fmt.Println()
-
-	dates := [3]time.Time{
-		time.Unix(0, 0),
-		time.Unix(1, 0),
-		time.Unix(194891203, 0), //need comma
-	}
-
-	// fmt.Println(dates[0], dates[1], dates[2])
-	// fmt.Println(dates)
-
-	// for i, date := range dates {
-	// 	fmt.Println(i, date)
-	// }
-
-	for _, date := range dates { //like python, safe
-		fmt.Println(date)
+	for _, score := range gpa {
+		fmt.Printf("%f ", score)
 	}
 }
