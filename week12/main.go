e1c1e84fb9851bc22d78d5fbd8aa4084010d9fb9
inhee <devforplayer@gmail.com>
2024-11-15 14:47:42 +0900

    v5.1 array for

--- week12/main.go
+++ week12/main.go
@@ -6,24 +6,16 @@ import (
 )
 
 func main() {
-
 	var scores [3]int
 
 	scores[1] = 90
-	fmt.Println(scores[1], scores[0], scores[2])
-	fmt.Printf("%#v\n", scores)
-	fmt.Println(scores)
 
-	// var dates [3]time.Time
+	//for i := 0; i < len(scores); i++ //<-panic: runtime error: index out of range [3] with length 3
+	for i := 0; i < len(scores); i++ { //unsafe
+		fmt.Printf("%d ", scores[i])
+	}
 
-	// dates[1] = time.Unix(198828330, 0)
-
-	// fmt.Println(dates[1])
-	// var scores [3]int
-
-	// scores[1] = 90
-
-	// fmt.Println(scores[1], scores[0], scores[2])
+	fmt.Println()
 
 	dates := [3]time.Time{
 		time.Unix(0, 0),
@@ -31,8 +23,14 @@ func main() {
 		time.Unix(194891203, 0), //need comma
 	}
 
-	fmt.Println(dates[0], dates[1], dates[2])
-	fmt.Println(dates)
+	// fmt.Println(dates[0], dates[1], dates[2])
+	// fmt.Println(dates)
 
-	// fmt.Printf("%#v\n", dates)
+	// for i, date := range dates {
+	// 	fmt.Println(i, date)
+	// }
+
+	for _, date := range dates { //like python, safe
+		fmt.Println(date)
+	}
 }
