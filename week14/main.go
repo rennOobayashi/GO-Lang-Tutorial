3c77470919f1ddc6cf0a25348aca8001d658ec17
inhee <devforplayer@gmail.com>
2024-11-29 14:50:54 +0900

    v8.2 type struct

--- week14/main.go
+++ week14/main.go
@@ -2,25 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	var student1 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
 
+func main() {
+	var student1 student
 	student1.id = 202444123
 	student1.name = "이인희"
 	student1.gpa = 4.42
 
 	fmt.Println(student1.gpa)
 
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
-
+	var student2 student
 	student2.id = 202444456
 	student2.name = "리인희"
 	student2.gpa = 2.44
