8e51f90eff165d5d1a1cd83f0c4d318145265787
inhee <devforplayer@gmail.com>
2024-11-29 14:43:01 +0900

    v8.1 struct

--- week14/main.go
+++ week14/main.go
@@ -3,25 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	ages := make(map[string]int)
-
-	var name string
-	var age int
-
-	for {
-		fmt.Print("Enter your name(quit to q): ")
-		fmt.Scanln(&name)
-		if name == "q" || name == "Q" {
-			break
-		}
-
-		fmt.Print("Enter your age: ")
-		fmt.Scanln(&age)
-
-		ages[name] = age
+	var student1 struct {
+		id   int
+		name string
+		gpa  float32
 	}
 
-	for name, age := range ages {
-		fmt.Printf("%s is %d years old.\n", name, age)
+	student1.id = 202444123
+	student1.name = "이인희"
+	student1.gpa = 4.42
+
+	fmt.Println(student1.gpa)
+
+	var student2 struct {
+		id   int
+		name string
+		gpa  float32
 	}
+
+	student2.id = 202444456
+	student2.name = "리인희"
+	student2.gpa = 2.44
+
+	fmt.Println(student2.id)
+
 }
